d53425e96a976a4549b6ed4d8895aa354b88fe04
dianlujitao <dianlujitao@lineageos.org>
2018-12-24 12:04:48 +0800

    wayne: Support Alipay fingerprint payment
     * IFAA manager is based on OnePlusOSS, but adapted for Xiaomi's mlipay
       interface.
     * vendor.xiaomi.hardware.mtdservice@1.0.so is not actually used, thus
       patchelf to drop it rather than shipping a blob.
    
    Change-Id: Idf3d3a8d40245984767f4ef5f60f9fe584e69f21
    
    Conflicts:
    	biometrics/BiometricsFingerprint.cpp
    	device.mk
    	extract-files.sh
    	fingerprint/BiometricsFingerprint.cpp
    	manifest.xml
    	sepolicy/file_contexts
    	sepolicy/hal_mlipay_default.te
    	sepolicy/property.te
    	sepolicy/property_contexts

--- platform.mk
+++ platform.mk
@@ -57,6 +57,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
 	external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# Alipay
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
+
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
+
 # Audio
 PRODUCT_PACKAGES += \
 	android.hardware.audio@4.0-impl:32 \
